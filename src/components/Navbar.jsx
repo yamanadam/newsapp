84f897618352988544d80295ca9bd6c1e340b570
Yamanadm <adsamu44@gmail.com>
2024-08-06 21:31:11 +0300

    navbar ok

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary p-3 ">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <span className="badge bg-danger">NEWS</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link   " aria-current="page" href="#">
+                Science
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sports
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Technology
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Business
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Health
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
